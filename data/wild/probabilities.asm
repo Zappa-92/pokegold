ebb478be70496cab52a474945b623e7ee51bea42
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2022-06-11 15:21:33 -0300

    Update probabilities.asm

--- data/wild/probabilities.asm
+++ data/wild/probabilities.asm
@@ -5,18 +5,18 @@ ENDM
 
 GrassMonProbTable:
 	table_width 2, GrassMonProbTable
-	mon_prob 25,  0 ; 25% chance
-	mon_prob 50,  1 ; 25% chance
-	mon_prob 70,  2 ; 20% chance
-	mon_prob 80,  3 ; 10% chance
-	mon_prob 88,  4 ;  8% chance
-	mon_prob 95,  5 ;  7% chance
-	mon_prob 100, 6 ;  5% chance
+	mon_prob 20,  0 ; 20% chance
+	mon_prob 40,  1 ; 20% chance
+	mon_prob 55,  2 ; 15% chance
+	mon_prob 67,  3 ; 12% chance
+	mon_prob 78,  4 ; 11% chance
+	mon_prob 89,  5 ; 11% chance
+	mon_prob 100, 6 ; 11% chance
 	assert_table_length NUM_GRASSMON
 
 WaterMonProbTable:
 	table_width 2, WaterMonProbTable
-	mon_prob 60,  0 ; 60% chance
-	mon_prob 90,  1 ; 30% chance
-	mon_prob 100, 2 ; 10% chance
+	mon_prob 50,  0 ; 50% chance
+	mon_prob 80,  1 ; 30% chance
+	mon_prob 100, 2 ; 20% chance
 	assert_table_length NUM_WATERMON
